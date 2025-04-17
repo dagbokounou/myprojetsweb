0e5b80e99481eb3cef95614da76a14c8bc36895d
dagbokounou <adeogratiasdonald@yahoo.fr>
2025-04-17 15:57:13 +0200

    jquery

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+
+
+    $("#p2").on({
+       mouseenter: function(){
+
+        $("#p4").hide();
+       },
+       mouseleave : function (){
+
+    $("#p4").show("slow");
+       }
+
+
+    })
+
+
+
+
+})
+
+
+ 
+
+
+
+     
+    
+    
+
+
+
